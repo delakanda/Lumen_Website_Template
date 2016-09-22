305a8c758fd7e49c41d27384ff83b98f539aba07
Delali Kanda <delakanda@gmail.com>
2016-09-22 12:54:28 +0000

    initial commit

---
+++ resources/views/layouts/home_master.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="author" content="">
+
+        <title>[ {{$title}} ] Company Name</title>
+
+        <link href="/css/master.css" rel="stylesheet" type="text/css">
+        <!--[if lt IE 9]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script src="/js/master.js"></script>
+    </head>
+
+    <body>
+
+    </body>
+
+</html>
